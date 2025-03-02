0e2c3e0785c581808d1b3af1d0a76dfd9249bf90
Ishan11032005GitHub <ishan11032005@gmail.com>
2025-03-02 18:47:44 +0530

    Updated files

---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function Nav() {
+  return (
+    <header>
+      <nav className="NB">
+        <div className="E_CELL_LOGO">I&E-CELL</div>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
